745db6d2043bd4b42c26b8a638a53e3afe6526c6
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-02-13 13:31:55 +0000

    created back end and set up seed data for testing, next will start on frontend, create log in page and test it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+trainer1 = Trainer.create(name: 'Mikey', password: '1', age: 27)
